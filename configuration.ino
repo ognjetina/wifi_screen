6597496799f9dc197bcdea9e577422a6ceb75334
ognjetina <ognjen.lazic@codecentric.de>
2018-02-10 17:38:01 +0100

    First commit

---
+++ configuration.ino
@@ -0,0 +1,105 @@
+// address map is having a 16 step
+// positions: 0,16,32,48,64,80,96,112,128,144,160,176,192,208,224,240,256,272,288
+//304,320,336,352,368,384,400,416,432,448,464,480,496
+
+const int configFlag = 0;
+const int passwordFlag = 16;
+const int wifiFlag = 32;
+// using address 80 since my board has problem with 48 to 80...
+const int wifiPassFlag = 80;
+// ----------------------------------------------------------------------  EEPROM
+void EEPROM_write_to_address(int address, String stringValue) {
+
+  char value[16];
+  stringValue.toCharArray(value, 16);
+
+  for (int i = 0; i < 16; i++) {
+    if (i < strlen(value)) {
+      // write to memory
+      EEPROM.write(address + i, (int)value[i]);
+    } else {
+      // clear rest of the memory
+      EEPROM.write(address + i, 0);
+    }
+    delay(20);
+  }
+  EEPROM.commit();
+}
+
+void EEPROM_clear() {
+  EEPROM.begin(512);
+  for (int i = 0 ; i < 512 ; i++) {
+    EEPROM.write(i, 0);
+    delay(10);
+  }
+  EEPROM.commit();
+}
+
+String EEPROM_read_from_address(int address) {
+  char temp;
+  int counter = 0;
+  String result = "";
+  EEPROM.begin(512);
+  for (int i = address; i < address + 16; i++) {
+    temp = EEPROM.read(i);
+    delay(50);
+    if (temp != NULL && int(temp) != 0) {
+      counter++;
+      result.concat(temp);
+    }
+  }
+  return result;
+}
+// ----------------------------------------------------------------------  configuration flag
+bool isConfigured() {
+  String isConfigured = EEPROM_read_from_address(configFlag);
+  isConfigured.trim();
+  Serial.println("Checking if device is configured...");
+  delay(10);
+  return isConfigured.equals("true");
+}
+
+void setToConfigured() {
+  EEPROM_write_to_address(configFlag, "true");
+  Serial.println("Configuration saved");
+  Serial.println("Restarting..");
+  delay(1500);
+  ESP.restart();
+}
+
+void destroyConfiguration() {
+  EEPROM_clear();
+  EEPROM_write_to_address(configFlag, "false");
+  Serial.println("Configuration destroyed!!");
+  Serial.println("Restarting..");
+  delay(1500);
+  ESP.restart();
+}
+// ----------------------------------------------------------------------  pass
+void set_password(String passowrd) {
+  Serial.println("Writing password");
+  EEPROM_write_to_address(passwordFlag, password);
+}
+String get_password() {
+  Serial.println("Reading password");
+  return EEPROM_read_from_address(passwordFlag);
+}
+// ----------------------------------------------------------------------  wifi
+void set_wifi(String wifi) {
+  Serial.println("Writing wifi");
+  EEPROM_write_to_address(wifiFlag, wifi);
+}
+String get_wifi() {
+  Serial.println("Reading wifi");
+  return EEPROM_read_from_address(wifiFlag);
+}
+
+// ----------------------------------------------------------------------  wifi pass
+void set_wifi_pass(String wifi_pass) {
+  Serial.println("Writing wifi pass");
+  EEPROM_write_to_address(wifiPassFlag, wifi_pass);
+}
+String get_wifi_pass() {
+  Serial.println("Reading wifi password");
+  return EEPROM_read_from_address(wifiPassFlag);
+}
---
+++ wifi_screen.ino
@@ -0,0 +1,208 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+StaticJsonBuffer<200> jsonBuffer;
+
+const char* username = "admin";
+char password[16] = "";
+char wifi[16];
+char wifi_pass[16];
+
+bool shouldPing = false;
+int refreshRate = 3000;
+String pingAddress = "";
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+ESP8266WebServer server(80);
+
+void writeToDisplay(String line1, String line2) {
+  lcd.clear();
+  lcd.print(line1);
+  lcd.setCursor(0, 1);
+  lcd.print(line2);
+}
+
+void handleDisplay() {
+  if (!server.authenticate(username, password))
+    return server.requestAuthentication();
+
+  if (server.arg("line1") == "" && server.arg("line2") == "") {
+    server.send(400, "text/plain", "Error");
+  } else {
+    server.send(200, "text/plain", "Lines recived");
+    writeToDisplay(server.arg("line1"), server.arg("line2"));
+  }
+}
+
+void handlePingStop() {
+  if (!server.authenticate(username, password))
+    return server.requestAuthentication();
+
+  lcd.clear();
+  shouldPing = false;
+  server.send(200, "text/plain", "Ping stop.");
+}
+
+void handlePing() {
+  if (!server.authenticate(username, password))
+    return server.requestAuthentication();
+
+  if (server.arg("pingAddress") == "" && server.arg("refreshRate") == "") {
+    server.send(400, "text/plain", "Error");
+  } else {
+    pingAddress = server.arg("pingAddress");
+    refreshRate = server.arg("refreshRate").toInt();
+    shouldPing = true;
+    server.send(200, "text/plain", "Ping start.");
+  }
+}
+
+void handleDestroy() {
+  if (!server.authenticate(username, password))
+    return server.requestAuthentication();
+
+  destroyConfiguration();
+  server.send(200, "text/plain", "Configuration destroyed");
+}
+void handleNotFound() {
+  server.send(404, "text/plain", "nothing to do here...");
+}
+
+void setup(void) {
+  Serial.begin(9600);
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.clear();
+
+  if (isConfigured()) {
+    Serial.println("Reading config");
+    get_wifi().toCharArray(wifi, 16);
+    get_wifi_pass().toCharArray(wifi_pass, 16);
+    get_password().toCharArray(password, 16);
+
+    lcd.clear();
+    lcd.print("Connecting to:");
+    lcd.setCursor(0, 1);
+    lcd.print(wifi);
+  } else {
+    writeToDisplay("Device is not", "configured!");
+    Serial.println("Waiting for configuration");
+
+    bool configured = false;
+    String configurationTemp = "";
+    String questions[4] = {"Input device password:", "Input wifi:", "Input wifi password:"};
+
+    while (!configured) {
+      Serial.println("Expecting configuration: device password,wifi,wifi_pass");
+      for (int i = 0; i < 3; i++) {
+        Serial.println(questions[i]);
+        while (Serial.available() == 0) {
+        }
+        configurationTemp = Serial.readString();
+        configurationTemp.trim();
+        if (configurationTemp.length() < 1) {
+          Serial.println("Invalid input");
+          i--;
+        } else {
+          Serial.print("You have inputed: ");
+          Serial.println(configurationTemp);
+          switch (i) {
+            case 0:
+              configurationTemp.toCharArray(password, 16);
+              break;
+            case 1:
+              configurationTemp.toCharArray(wifi, 16);
+              break;
+            case 2:
+              configurationTemp.toCharArray(wifi_pass, 16);
+              configured = true;
+              break;
+            default:
+              Serial.println("This is imposiburu!");
+          }
+
+        }
+      }
+      Serial.println("------------------------");
+      Serial.println("Configuration done.");
+      Serial.print("Device Password: ");
+      Serial.println(password);
+      Serial.print("Wifi: ");
+      Serial.println(wifi);
+      Serial.print("Wifi password: ");
+      Serial.println(wifi_pass);
+      Serial.println("------------------------");
+
+    }
+    Serial.println("Saving data to eeprom");
+    set_password(password);
+    set_wifi(String(wifi));
+    set_wifi_pass(String(wifi_pass));
+    setToConfigured();
+    Serial.println("------------------------");
+  }
+
+  WiFi.begin(wifi, wifi_pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Connected");
+
+  lcd.clear();
+  lcd.print("wifi: ");
+  lcd.print(wifi);
+  lcd.setCursor(0, 1);
+  lcd.print("IP: ");
+  lcd.print(WiFi.localIP());
+
+
+  server.on("/display", handleDisplay);
+  server.on("/destroy", handleDestroy);
+  server.on("/ping", handlePing);
+  server.on("/pingStop", handlePingStop);
+  server.onNotFound(handleNotFound);
+  server.begin();
+}
+
+void loop(void) {
+  ArduinoOTA.handle();
+  server.handleClient();
+
+  while (shouldPing) {
+    Serial.print("Pinging: ");
+    Serial.println(pingAddress);
+    Serial.print("At rate: ");
+    Serial.println(refreshRate);
+    HTTPClient http;
+    http.begin(pingAddress);
+    int httpCode = http.GET();                                                                  //Send the request
+
+    if (httpCode = 200) {
+      JsonObject& root = jsonBuffer.parseObject(http.getString());
+      if (!root.success()) {
+        Serial.println("Serial parse failed");
+        return;
+      } else {
+        writeToDisplay(root["line1"], root["line2"]);
+        jsonBuffer.clear();
+      }
+    } else {
+      writeToDisplay("Something went", "wrong!!!")
+    }
+    http.end();
+    delay(refreshRate);
+    ArduinoOTA.handle();
+    server.handleClient();
+  }
+
+}
